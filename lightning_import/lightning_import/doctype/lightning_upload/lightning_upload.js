275e032ff3adc874e48d563232b1daa17e954119
shahzadbinshahajhan <shahzadbinshahjahan@gmail.com>
2025-06-05 07:53:50 +0000

    hide start import

--- lightning_import/lightning_import/doctype/lightning_upload/lightning_upload.js
+++ lightning_import/lightning_import/doctype/lightning_upload/lightning_upload.js
@@ -9,22 +9,26 @@
 
 frappe.ui.form.on('Lightning Upload', {
     refresh(frm) {
-      if (!frm.is_new()) {
-        frm.page.set_primary_action(__('Start Import'), () => {
-          // Your custom action
-          frappe.call({
-            method: 'your_app.api.start_import',
-            args: {
-              docname: frm.doc.name
-            },
-            callback: function(r) {
-              if (!r.exc) {
-                frappe.msgprint(__('Import started successfully'));
-              }
-            }
-          });
-        });
-      }
+        // Show Start Import button only when status is Draft and document is saved
+        if (!frm.is_new() && frm.doc.status === "Draft") {
+            frm.page.set_primary_action(__('Start Import'), () => {
+                frappe.call({
+                    method: 'lightning_import.lightning_import.doctype.lightning_upload.lightning_upload.start_import',
+                    args: {
+                        docname: frm.doc.name
+                    },
+                    callback: function(r) {
+                        if (!r.exc) {
+                            frappe.msgprint(__('Import started successfully'));
+                            frm.reload_doc();
+                        }
+                    }
+                });
+            });
+        } else {
+            // Remove the primary action button if conditions are not met
+            frm.page.clear_primary_action();
+        }
     }
-  });
+});
   
