d6b06833fdc5146c76754fdce0c8e6db21e4a28b
Shahzad Bin Shahjahan <shahzadbinshahjahan@gmail.com>
2025-06-08 23:54:27 +0530

    settings and lightning upload fields update

---
+++ lightning_import/lightning_import/doctype/lightning_upload_settings/lightning_upload_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Tridz Technologies Pvt Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Lightning Upload Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
