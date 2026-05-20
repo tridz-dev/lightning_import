868c860c3e8a5e9b6f6d7f2c91f37c2224c38df0
Ameen <ameen@erpcode.com>
2026-05-20 11:04:54 +0000

    fix: validate update imports using mapped custom update fields

--- lightning_import/lightning_import/doctype/lightning_upload/test_lightning_upload.py
+++ lightning_import/lightning_import/doctype/lightning_upload/test_lightning_upload.py
@@ -1,9 +1,2 @@
 # Copyright (c) 2025, Tridz Technologies Pvt Ltd and Contributors
 # See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestLightningUpload(FrappeTestCase):
-	pass
